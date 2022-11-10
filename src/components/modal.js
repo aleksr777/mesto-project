4d7839d84b23f605f95c71734db0bf6ab99718c3
Aleksandr Petrov <petrov.aleksr@gmail.com>
2022-11-10 23:45:42 +0500

    refactor: изменён способ отображения анимации открытия и закрытия popup

--- src/components/modal.js
+++ src/components/modal.js
@@ -6,19 +6,30 @@ const handlerEsc = (event) => {
 }
 
 const openPopup = (popup) => {
+  popup.style.pointerEvents = 'none';
+  popup.style.transition = 'all .4s ease 0s';
+  popup.style.opacity = '0'; // делаем popup изначально прозрачным
   popup.classList.add('popup_opened');
-  document.addEventListener('keydown', handlerEsc);
+  // далее плавно делаем popup непрозрачным (setTimeout нужен для срабатывания свойства transition)
+  setTimeout(() => popup.style.opacity = '1', 0);
+  setTimeout(() => {
+    document.addEventListener('keydown', handlerEsc);
+    popup.style.pointerEvents = '';
+    popup.style.transition = '';
+  }, 400); 
 };
 
 const closePopup = (popup) => {
+  popup.style.pointerEvents = 'none';
   document.removeEventListener('keydown', handlerEsc);
-  popup.style.animationName = 'close-popup';
-  popup.style.animationPlayState = 'running';
-  setTimeout(() => {
+  popup.style.transition = 'all .4s ease 0s';
+  popup.style.opacity = '0'; // делаем popup плавно прозрачным (срабатывает свойство transition)
+  setTimeout(() => { // setTimeout нужен, чтобы успела сработать анимация
     popup.classList.remove('popup_opened');
-    popup.style.animationName = '';
-    popup.style.animationPlayState = '';
-  }, 300);
+    popup.style.opacity = ''; // возвращаем значения по-умолчанию
+    popup.style.pointerEvents = '';
+    popup.style.transition = '';
+  }, 400);
 };
 
 const closeCurrentPopup = (event) => {
